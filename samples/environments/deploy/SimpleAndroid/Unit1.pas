dc94f7b10bbe73960c47ac650c6ae489fb547ddd
lmbelo <lucas.belo@live.com>
2022-10-28 14:09:34 -0300

    New samples
    Former-commit-id: d815dbad0e81c7fcc5dbd594ecea99d0c42f50a1 [formerly 81100cd2e9a9c295b20e184425f7f71d7787a833] [formerly cb8bfbf7837fcf8361d904ed2c1a0356123ab3ed [formerly 3c33d8548770b775017bd73b662f196043e1c7d7]] [formerly e8efc964137c1df8903bf67ba36b7b1310acf401 [formerly fd7302bcb4d5c3d85f923c22c15b39322594eb2e] [formerly ec6c2f12a89b8ca7104911c6837fd8b4eccf7fed [formerly 71423b545f49f69284b8d8e2d1b676348efe0b2d]]]
    Former-commit-id: 7ae667aaca62eecf084d860e19332ee879ab37f5 [formerly f8a99dd3c4a76bcc08d3bdd4e068df4b1cc637cc] [formerly 5f8b185b52494fdff0e31353a96a4defe281fb5c [formerly ecb237fb000a3461793f18228ea81b4967d7bfa4]]
    Former-commit-id: c8c4c2a9c896295afdaa93566f16c3b9d572955f [formerly def5ebf825d412a19e07da4b6467c05c6c9aeefa]
    Former-commit-id: 5384f01d50340ef276ecc35488a1175160ac3394
    Former-commit-id: 28afe185ca9d232a3642c86cbc8dff5b16866456

--- samples/environments/deploy/SimpleAndroid/Unit1.pas
+++ samples/environments/deploy/SimpleAndroid/Unit1.pas
@@ -5,16 +5,14 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Memo.Types,
-  FMX.Controls.Presentation, FMX.ScrollBox, FMX.Memo, PythonEngine,
-  PyEnvironment, PyEnvironment.Embeddable, FMX.StdCtrls, System.Zip;
+  FMX.Controls.Presentation, FMX.ScrollBox, FMX.Memo, FMX.Platform, PythonEngine,
+  PyEnvironment, PyEnvironment.Embeddable;
 
 type
   TForm1 = class(TForm)
     PythonEngine1: TPythonEngine;
     Memo1: TMemo;
     PyEmbeddedEnvironment1: TPyEmbeddedEnvironment;
-    StatusBar1: TStatusBar;
-    Label1: TLabel;
     procedure PyEmbeddedEnvironment1AfterActivate(Sender: TObject;
       const APythonVersion: string; const AActivated: Boolean);
     procedure PyEmbeddedEnvironment1AfterSetup(Sender: TObject;
@@ -29,14 +27,9 @@ type
       const APythonVersion: string);
     procedure FormCreate(Sender: TObject);
     procedure FormCloseQuery(Sender: TObject; var CanClose: Boolean);
-    procedure PyEmbeddedEnvironment1ZipProgress(Sender: TObject;
-      ADistribution: TPyCustomEmbeddableDistribution; FileName: string;
-      Header: TZipHeader; Position: Int64);
   private
-    FAsyncActivate: IAsyncResult;
-    { Private declarations }
-  public
-    { Public declarations }
+    FAsyncSetup: IAsyncResult;
+    function AppEventHandler(AAppEvent: TApplicationEvent; AContext: TObject): boolean;
   end;
 
 var
@@ -44,75 +37,87 @@ var
 
 implementation
 
-uses
-  System.IOUtils;
-
 {$R *.fmx}
+
+function TForm1.AppEventHandler(AAppEvent: TApplicationEvent;
+  AContext: TObject): boolean;
+begin
+  case AAppEvent of
+    TApplicationEvent.FinishedLaunching: begin
+      if PyEmbeddedEnvironment1.EndSetup(FAsyncSetup) then begin
+        PyEmbeddedEnvironment1.Activate();
+      end;
+    end;
+  end;
+  Result := true;
+end;
 
 procedure TForm1.FormCloseQuery(Sender: TObject; var CanClose: Boolean);
 begin
-  CanClose := FAsyncActivate.IsCompleted;
-  if not CanClose then
-    ShowMessage('Waiting for background operation. Try again.');
+  if not FAsyncSetup.IsCompleted then
+    FAsyncSetup.Cancel();
+
+  FAsyncSetup.AsyncWaitEvent.WaitFor();
 end;
 
 procedure TForm1.FormCreate(Sender: TObject);
-var
-  LAsyncSetup: IAsyncResult;
 begin
-  LAsyncSetup := PyEmbeddedEnvironment1.SetupAsync();
-  FAsyncActivate := PyEmbeddedEnvironment1.ActivateAsync(LAsyncSetup);
+  FAsyncSetup := PyEmbeddedEnvironment1.BeginSetup();
+  var LAppEventSErvice := IFMXApplicationEventService(nil);
+  if TPlatformServices.Current.SupportsPlatformService(IFMXApplicationEventService, IInterface(LAppEventService)) then
+    LAppEventService.SetApplicationEventHandler(AppEventHandler);
+
   Memo1.Lines.Add('Background task has started.');
 end;
 
 procedure TForm1.PyEmbeddedEnvironment1AfterActivate(Sender: TObject;
   const APythonVersion: string; const AActivated: Boolean);
 begin
-  Memo1.Lines.Add(Format('Python %s has been activated.', [APythonVersion]));
+  TThread.ForceQueue(TThread.Current, procedure() begin
+    Memo1.Lines.Add(Format('Python %s has been activated.', [APythonVersion]));
+  end);
 end;
 
 procedure TForm1.PyEmbeddedEnvironment1AfterSetup(Sender: TObject;
   const APythonVersion: string);
 begin
-  Memo1.Lines.Add(Format('Python %s has been setup.', [APythonVersion]));
+  TThread.ForceQueue(TThread.Current, procedure() begin
+    Memo1.Lines.Add(Format('Python %s has been setup.', [APythonVersion]));
+  end);
 end;
 
 procedure TForm1.PyEmbeddedEnvironment1BeforeActivate(Sender: TObject;
   const APythonVersion: string);
 begin
-  Memo1.Lines.Add(Format('Activating Python %s.', [APythonVersion]));
+  TThread.ForceQueue(TThread.Current, procedure() begin
+    Memo1.Lines.Add(Format('Activating Python %s.', [APythonVersion]));
+  end);
 end;
 
 procedure TForm1.PyEmbeddedEnvironment1BeforeSetup(Sender: TObject;
   const APythonVersion: string);
 begin
-  Memo1.Lines.Add(Format('Setting up Python %s.', [APythonVersion]));
+  TThread.ForceQueue(TThread.Current, procedure() begin
+    Memo1.Lines.Add(Format('Setting up Python %s.', [APythonVersion]));
+  end);
 end;
 
 procedure TForm1.PyEmbeddedEnvironment1Error(Sender: TObject;
   const AException: Exception);
 begin
-  Memo1.Lines.Add(Format(
-    'An error occurred during the installation process: %s', [
-    AException.ToString()]));
+  TThread.ForceQueue(TThread.Current, procedure() begin
+    Memo1.Lines.Add(Format(
+      'An error occurred during the installation process: %s', [
+      AException.ToString()]));
+  end);
 end;
 
 procedure TForm1.PyEmbeddedEnvironment1Ready(Sender: TObject;
   const APythonVersion: string);
 begin
-  Memo1.Lines.Add(Format('Python %s is ready.', [APythonVersion]));
-end;
-
-procedure TForm1.PyEmbeddedEnvironment1ZipProgress(Sender: TObject;
-  ADistribution: TPyCustomEmbeddableDistribution; FileName: string;
-  Header: TZipHeader; Position: Int64);
-begin
-  //Zip progress is neer synchronized, even when
-  //the SynchronizeEvents property is set to true
-  TThread.Queue(nil, procedure() begin
-    Label1.Text := FileName.Replace(
-      TDirectory.GetParent(ADistribution.EnvironmentPath), String.Empty, []);
-  end)
+  TThread.ForceQueue(TThread.Current, procedure() begin
+    Memo1.Lines.Add(Format('Python %s is ready.', [APythonVersion]));
+  end);
 end;
 
 end.
