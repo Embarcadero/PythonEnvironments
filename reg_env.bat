a5babe9837b305844e5805fd14b757312ea60803
lmbelo <lucas.belo@live.com>
2022-11-01 10:56:24 -0300

    Script to set the PYTHONENVIRONMENTDIR env var

---
+++ reg_env.bat
@@ -0,0 +1,14 @@
+@echo off
+
+set PRODVER=22.0
+set PYENVPATH=%cd%
+
+if not "%~1"=="" (SET PRODVER=%~1)
+if not "%~2"=="" (SET PYENVPATH=%~2)
+
+//A trick to solve relative path issues
+pushd %PYENVPATH%
+set PYENVPATH=%cd%
+popd
+
+reg add "HKEY_CURRENT_USER\Software\Embarcadero\BDS\%PRODVER%\Environment Variables" /v PYTHONENVIRONMENTDIR /d %PYENVPATH% /f
